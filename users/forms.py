5f0baf050fc8de809845e744ae53f03cd04bff3a
Oduntan Balogun <oduntan@live.com>
2019-04-25 22:57:19 +0100

    Removed Login form

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,6 @@
 from django import forms
 from .models import User
 
-
-class LoginForm(forms.Form):
-    username = forms.CharField(required=True)
-    password = forms.CharField(required=True, widget=forms.PasswordInput())
-
-
 class UserModelForm(forms.ModelForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
