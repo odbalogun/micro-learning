93df857f03adecd3c0de256d4738a4c2a9976206
Oduntan Balogun <oduntan@live.com>
2019-04-25 21:32:12 +0100

    Added UserCreateView

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import LoginForm
+from .forms import LoginForm, UserModelForm
 
 
 class LoginView(FormView):
     form_class = AuthenticationForm
     template_name = "users/login.html"
 
+
+class UserCreateView(CreateView):
+    form_class =  UserModelForm
+    template_name = "users/create.html"
