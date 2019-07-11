dab5f0440bb26f4ed446ba0aa10cde1ab3c2898c
Oduntan Balogun <oduntan@live.com>
2019-07-11 12:25:20 +0100

    work in progress

--- payments/models.py
+++ payments/models.py
@@ -5,10 +5,13 @@ from courses.models import Courses, Enrolled
 
 # Create your models here.
 class PaymentLog(models.Model):
-    enrolled = models.ForeignKey(Enrolled, on_delete=models.SET_NULL, null=True)
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='payment_log')
     amount_paid = models.DecimalField('amount paid', decimal_places=2, max_digits=10, default=0)
     note = models.TextField('note', null=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField('created at', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'payment'
+        verbose_name_plural = 'payments'
