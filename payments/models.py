c20c2c115d95c953575ad7e30a15d8a718b60057
Oduntan Balogun <oduntan@live.com>
2019-08-22 19:19:05 +0100

    Updated payment model

--- payments/models.py
+++ payments/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings
-from courses.models import Courses, Enrolled
+from courses.models import Enrolled
 
 
 # Create your models here.
 class PaymentLog(models.Model):
-    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='payment_log')
+    enrolled = models.ForeignKey(Enrolled, on_delete=models.SET_NULL, null=True)
     amount_paid = models.DecimalField('amount paid', decimal_places=2, max_digits=10, default=0)
+    payment_type = models.CharField('payment type', max_length=50)
+    reference_no = models.CharField('reference no', max_length=50, null=True)
     note = models.TextField('note', null=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField('created at', auto_now_add=True)
